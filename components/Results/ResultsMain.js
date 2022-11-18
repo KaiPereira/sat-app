db8f94c43dda8208718ecbb0341b18a98608828b
avichal <avichalwebdev@gmail.com>
2022-11-19 00:54:17 +0530

    completed the homepage

--- components/Results/ResultsMain.js
+++ components/Results/ResultsMain.js
@@ -1,40 +1,32 @@
-import React from "react"
-import classes from "./ResultsMain.module.scss"
-import Answer from "./Answer"
+import React from "react";
+import classes from "./ResultsMain.module.scss";
+import Answer from "./Answer";
 
 export default function ResultsMain() {
-    const [score, changeScore] = React.useState([])
-    
-    React.useEffect(() => {
-        if (localStorage.getItem("score")) {
-            changeScore(JSON.parse(localStorage.getItem("score")))
-        }
-    }, [])
+   const [score, changeScore] = React.useState([]);
 
-    
-    const allScore = score.map((answer, index) => {
-        return (
-            <Answer 
-                questionNum={index}
-                score={score[index]}
-            />
-        )
-    })
-    return (
-        <>
-        { score.length !== 0 ?
-        <main className={classes.main}>
-            <h1 className={classes.answersHeader}>Answers</h1>
-            <div className={classes.answerMain}>
-                {allScore}
-            </div>
-        </main>
-        :
-        <main className={classes.main}>
-            <h1 className={classes.answersHeader}>Answers</h1>
-            <p className={classes.noAnswersText}>Currently No Answers</p>
-        </main>
-        }
-        </>
-    )
+   React.useEffect(() => {
+      if (localStorage.getItem("score")) {
+         changeScore(JSON.parse(localStorage.getItem("score")));
+      }
+   }, []);
+
+   const allScore = score.map((answer, index) => {
+      return <Answer key={Math.random()} questionNum={index} score={score[index]} />;
+   });
+   return (
+      <>
+         {score.length !== 0 ? (
+            <main className={classes.main}>
+               <h1 className={classes.answersHeader}>Answers</h1>
+               <div className={classes.answerMain}>{allScore}</div>
+            </main>
+         ) : (
+            <main className={classes.main}>
+               <h1 className={classes.answersHeader}>Answers</h1>
+               <p className={classes.noAnswersText}>Currently No Answers</p>
+            </main>
+         )}
+      </>
+   );
 }
