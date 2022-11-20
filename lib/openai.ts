1f9ae59df1b88928a19085168c80381c096f8d73
Sunrit Jana <warriordefenderz@gmail.com>
2022-11-20 16:15:08 +0530

    Complete the lib code for openai

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,1 +1,23 @@
-export class OpenAIWrapper {}
+import { Configuration, OpenAIApi } from "openai";
+
+export class OpenAIWrapper {
+    public openai: OpenAIApi;
+
+    constructor(apiKey: string) {
+        const configuration = new Configuration({
+            apiKey,
+        });
+
+        this.openai = new OpenAIApi(configuration);
+    }
+
+    public async generate(prompt: string, maxTokens: number) {
+        const response = await this.openai.createCompletion({
+            model: "text-davinci-002",
+            prompt,
+            max_tokens: maxTokens,
+        });
+
+        return response.data.choices[0].text;
+    }
+}
